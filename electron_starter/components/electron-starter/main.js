8cecb7f53802c9552383369c48f3fe5dc2000bd5
Matthew van der Hoorn <md.registrations@protonmail.com>
2025-01-23 13:41:06 +0100

    Alpha

--- electron_starter/components/electron-starter/main.js
+++ electron_starter/components/electron-starter/main.js
@@ -1,1 +1,22 @@
-console.log("Starting");
+const { app, BrowserWindow } = require('electron');
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600
+    });
+
+    win.loadFile('electron_starter/components/electron-starter/index.html');
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+})
